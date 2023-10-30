681856cc145610f9319d60ab2847b78cd8d46d72
leofiorotto <leo.fiorotto@hotmail.com>
2023-10-29 21:18:31 -0300

    section6

---
+++ my-react-app/src/components/Seccion6/Seccion6.jsx
@@ -0,0 +1,22 @@
+function Seccion6() {
+  return (
+    <div className="flex mb-32 mt-20 bg-orange-600">
+      
+        <img src="https://media-public.canva.com/kCUrg/MAD0WHkCUrg/1/s.jpg" alt="imagen-seccion6" className="w-1/2"/>
+      
+      <div className="">
+          <div className="   ml-40">
+            <h1 className= "max-w-2xl -rotate-90 text-black font-bold text-8xl flex items-start  h-screen"> SALUD EN <br>
+            </br>MOVIMIENTO </h1>
+            <h1 className="-rotate-90 text-black font-bold text-8xl"> </h1>
+        
+          <h3 className="text-white ">LA ACTIVIDAD FISICA ES INNEGOCIABLE PARA UNA VIDA SALUDABLE</h3>
+          </div>
+        
+      </div>
+
+    </div>
+  );
+}
+
+export default Seccion6;

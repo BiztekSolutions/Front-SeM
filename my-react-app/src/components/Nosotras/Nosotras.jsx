911d71d6e37434660cc413aa46aece6a67abedf4
Leof098 <70485779+Leof098@users.noreply.github.com>
2023-11-01 16:35:31 -0300

    update nuestro equipo

--- my-react-app/src/components/Nosotras/Nosotras.jsx
+++ my-react-app/src/components/Nosotras/Nosotras.jsx
@@ -36,16 +36,41 @@ function Nosotras() {
   }
   const HeaderHeight = 112;
   return (
-    <div className="flex pb-14 py-14" style={{ minHeight: "617px" }}>
-      <div className="w-1/2 p-8">
-        <h1 className="text-5xl  text-customOrange font-bold m-10 mb-20">
-          NUESTRO EQUIPO
-        </h1>
-        <h3 className="text-lg my-4 text-center mb-28 w-3/5 m-auto">
-          Contamos con un equipo de profesionales dispuestos a ayudarte en cada
-          paso del proceso de entrenamiento. Nuestro objetivo es que te sientas
-          a gusto y puedas lograr tus objetivos.
-        </h3>
+    <div style={{ height: "900px" }}>
+      <div className="pt-16">
+        <div>
+          <h1 className="text-5xl  text-customOrange font-bold ">
+            NUESTRO EQUIPO
+          </h1>
+          <h3 className="text-lg my-4 text-center mb-28 w-3/5 m-auto">
+            Contamos con un equipo de profesionales dispuestos a ayudarte en
+            cada paso del proceso de entrenamiento. Nuestro objetivo es que te
+            sientas a gusto y puedas lograr tus objetivos.
+          </h3>
+        </div>
+        <div className="relative flex justify-center content-center mb-20">
+          <TeamMember
+            id="card1"
+            nombre="Florencia Peix"
+            edad={24}
+            descripcion="Soy Licenciada en Educación Fisica. Soy dueña de este gimnasio. Apasionada por el deporte y la vida sana. Me gusta mucho el deporte y la vida sana. Ejerzo esta profesión hace 10 años."
+            ubicacion="Saladillo, Buenos Aires, Argentina"
+          />
+          <TeamMember
+            id="card2"
+            nombre="Milagros Mastantuono"
+            edad={24}
+            descripcion="Soy Licenciada en Educación Fisica. Soy dueña de este gimnasio. Apasionada por el deporte y la vida sana. Me gusta mucho el deporte y la vida sana. Ejerzo esta profesión hace 10 años."
+            ubicacion="Saladillo, Buenos Aires, Argentina"
+          />
+          <TeamMember
+            id="card3"
+            nombre="Otro loco"
+            edad={30}
+            descripcion=" ESTE OTRO LOCO ANDA A SABER QUE HACE Soy Licenciada en Educación Fisica. Soy dueña de este gimnasio. Apasionada por el deporte y la vida sana. Me gusta mucho el deporte y la vida sana. Ejerzo esta profesión hace 10 años."
+            ubicacion="Saladillo, Buenos Aires, Argentina"
+          />
+        </div>
         <Link
           offset={-HeaderHeight}
           className=" bg-customOrange hover:bg-customOrange font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 cursor-pointer"
@@ -63,20 +88,6 @@ function Nosotras() {
             <FaWhatsapp className="m-2 h-12 w-12 cursor-pointer" />
           </div>
         </div>
-      </div>
-      <div className="flex justify-center gap-10">
-        <TeamMember
-          nombre="Florencia Peix"
-          titulo="Licenciada en Ed. Fisica"
-          educacion="Aquí va información sobre tu educación."
-          contacto="Aquí va información sobre cómo contactarte."
-        />
-        <TeamMember
-          nombre="Milagros Mastantuono"
-          titulo="Licenciada en Ed. Fisica"
-          educacion="Aquí va información sobre tu educación."
-          contacto="Aquí va información sobre cómo contactarte."
-        />
       </div>
     </div>
   );
--- my-react-app/src/components/Nosotras/TeamMember.jsx
+++ my-react-app/src/components/Nosotras/TeamMember.jsx
@@ -1,40 +1,62 @@
-import { useState } from "react";
+import React from "react";
+import "./TeamMember.css";
+import { CgProfile } from "react-icons/cg";
 
-import { IoPersonCircleOutline } from "react-icons/io5";
-
-const TeamMember = ({ nombre, titulo, educacion, contacto }) => {
-  const [mostrarImagen, setMostrarImagen] = useState(false);
-
-  const toggleImagen = () => {
-    setMostrarImagen(!mostrarImagen);
-  };
+const TeamMember = (props) => {
+  const { id, nombre, edad, descripcion, ubicacion } = props;
 
   return (
-    <div className="flex flex-col text-center justify-center content-center">
-      <IoPersonCircleOutline className="w-24 h-24" />
-      <div>
-        <h1 className="text-2xl">{nombre}</h1>
-        <span>{titulo}</span>
-        <div>
-          <button className="bg-customOrange" onClick={toggleImagen}>
-            Saber mas de mi
-          </button>
-          {mostrarImagen && (
-            <div className="border-white h-auto p-4 rounded-lg shadow-lg bg-gray-100">
-              <div className="max-w-fit m-auto bg-customOrange p-4 rounded-lg">
-                <div className="mb-4">
-                  <h3 className="text-xl font-bold mb-2">Mi educación</h3>
-                  <hr className="border-red-500 mb-2" />
-                  <p>{educacion}</p>
-                </div>
-                <div className="mb-4">
-                  <h3 className="text-xl font-bold mb-2">Mi contacto</h3>
-                  <hr className="border-red-500 mb-2" />
-                  <p>{contacto}</p>
+    <div className="wrapper">
+      <div className="card">
+        <input type="checkbox" id={id} className="more" aria-hidden="true" />
+        <div className="content">
+          <div
+            className="front"
+            style={{
+              backgroundImage:
+                "url('https://png.pngtree.com/background/20210710/original/pngtree-technological-sense-business-line-background-picture-image_1014566.jpg')",
+            }}
+          >
+            <div className="inner">
+              <CgProfile className="h-28 w-28 text-black" />
+              <h2 className="text-black">{nombre}</h2>
+              <div className="rating">
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="far fa-star"></i>
+              </div>
+              <label htmlFor={id} className="button" aria-hidden="true">
+                Details
+              </label>
+            </div>
+          </div>
+          <div className="back text-black">
+            <div className="inner">
+              <div className="info">
+                <span>{edad}</span>
+                <div className="icon">
+                  <i className="fas fa-users"></i>
+                  <span>Años</span>
                 </div>
               </div>
+
+              <div className="description">
+                <p>{descripcion}</p>
+              </div>
+              <div className="location">{ubicacion}</div>
+              <div className="price">38€ / day</div>
+              <label
+                htmlFor={id}
+                className="button return text-black"
+                aria-hidden="true"
+              >
+                Volver
+                <i className="fas fa-arrow-left"></i>
+              </label>
             </div>
-          )}
+          </div>
         </div>
       </div>
     </div>

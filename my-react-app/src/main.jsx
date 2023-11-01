499d3602ac7fd77bc16fb4527c69031ecf141ceb
Iñaki Armendariz <iniakitoo@gmail.com>
2023-10-31 22:29:33 -0300

    coach

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-
 import "tailwindcss/tailwind.css";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";

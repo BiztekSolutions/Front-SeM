7e6b84ab4cdb4181e0b8bb1fac434ac9dbb76fdd
ArianCalabrese <ariancalabrese@hotmail.com>
2023-11-27 19:48:41 -0300

    Added loading spinner

--- frontend/src/Views/Coach/SingleUser/User.jsx
+++ frontend/src/Views/Coach/SingleUser/User.jsx
@@ -1,21 +1,19 @@
-import { useSelector } from "react-redux";
 import { useEffect } from "react";
-import styles from "./SingleUser.module.css";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 import { FcFullTrash } from "react-icons/fc";
-import { useDispatch } from "react-redux";
-import { deleteUser, getUser } from "../../../features/user/userSlice";
-import { TailSpin } from "react-loader-spinner";
 import { MdOutlinePerson2 } from "react-icons/md";
-import { Typography } from 'antd';
+import { Typography } from "antd";
+import { deleteUser, getUser } from "../../../features/user/userSlice";
+import LoadingSpinner from "@/shared/components/spinner/LoadingSpinner";
+
+import styles from "./SingleUser.module.css";
 
 export default function User() {
   const dispatch = useDispatch();
-  const navigate = useNavigate();
   const user = useSelector((state) => state.users.user);
   const { id } = useParams();
   const userId = id;
-  const handleDelete = (userName, userId) => {};
 
   useEffect(() => {
     if (userId) {
@@ -23,25 +21,22 @@ export default function User() {
     }
   }, []);
 
+  const handleDelete = (userName, userId) => {};
+
   return (
     <div>
       {!user.Credentials ? (
-        <p>Cargando</p>
+        <LoadingSpinner />
       ) : (
         <div
           className={`${styles.wrapper} p-6 rounded-lg shadow-lg userDetails`}
         >
           <div className={styles.title}>
-            <div
-              className={`${styles.goBack} my-4`}
-              onClick={() => navigate("/coach")}
-            >
-              <div className="ml-5 text-4xl font-bold font-barlow-regular flex gap-4">
-                <MdOutlinePerson2 className="rounded-full h-15 w-15" />
-                <Typography.Title level={3} className="text-center">
-                  Perfil de {user.name}
-                </Typography.Title>
-              </div>
+            <div className="ml-5 text-4xl font-bold font-barlow-regular flex gap-4">
+              <MdOutlinePerson2 className="rounded-full h-15 w-15" />
+              <Typography.Title level={3} className="text-center">
+                Perfil de {user.name}
+              </Typography.Title>
             </div>
           </div>
           <div className="w-full">
@@ -50,29 +45,41 @@ export default function User() {
               className={styles.customDiv + "p-4 rounded-lg shadow mb-4 ml-5"}
             >
               <div className="mb-2 w-fit flex gap-5">
-                <h3 className=" text-left text-3xl">Nombre:</h3>
+                <Typography.Title level={3} className="text-left">
+                  Nombre:
+                </Typography.Title>
                 <b className="text-left text-3xl font-bold">{user.name}</b>
               </div>
               <div className="mb-2 w-fit flex gap-5">
-                <h3 className=" text-left text-3xl ">Apellido:</h3>
+                <Typography.Title level={3} className="text-left ">
+                  Apellido:
+                </Typography.Title>
                 <b className="text-left text-3xl font-bold">{user.lastname}</b>
               </div>
               <div className="mb-2 w-fit flex gap-5">
-                <h3 className=" text-left text-3xl">Email:</h3>
+                <Typography.Title level={3} className="text-left">
+                  Email:
+                </Typography.Title>
                 <b className="text-left text-3xl font-bold">
                   {user.Credentials[0].email}
                 </b>
               </div>
               <div className="mb-2 w-fit flex gap-5">
-                <h3 className="text-left text-3xl">Grupo:</h3>
+                <Typography.Title level={3} className="text-left text-3xl">
+                  Grupo:
+                </Typography.Title>
                 <b className="text-left text-3xl font-bold">Grupo</b>
               </div>
               <div className="mb-2 w-fit flex gap-5">
-                <h3 className="text-left text-3xl">Status:</h3>
+                <Typography.Title level={3} className="text-left text-3xl">
+                  Status:
+                </Typography.Title>
                 <b className="text-left text-3xl font-bold">Estatus</b>
               </div>
               <div className="mb-2 w-fit flex gap-5">
-                <h3 className="text-left text-3xl">Eliminar:</h3>
+                <Typography.Title level={3} className="text-left text-3xl">
+                  Eliminar:
+                </Typography.Title>
                 <b className="text-left text-3xl font-bold mt-2 cursor-pointer ">
                   <FcFullTrash
                     size={20}
@@ -87,17 +94,4 @@ export default function User() {
       )}
     </div>
   );
-  // return (
-  //   <div>
-  //     {!user.Credentials ? (
-  //       <p>Cargando</p>
-  //     ) : (
-  //       <div>
-  //         <h1>{user.name}</h1>
-  //         <h1>{user.lastname}</h1>
-  //         <h1>{user.Credentials[0].email}</h1>
-  //       </div>
-  //     )}
-  //   </div>
-  // );
 }

3351358324c631ad5d197ba301fb506f21907204
inaki <iniakitoo@gmail.com>
2024-02-29 15:12:56 -0300

    form fix

--- frontend/src/components/Formulario/Form.jsx
+++ frontend/src/components/Formulario/Form.jsx
@@ -1,5 +1,5 @@
 import { useForm } from "react-hook-form";
-
+import axios from "axios";
 function ContactForm() {
   const {
     register,
@@ -7,7 +7,23 @@ function ContactForm() {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => {};
+  const onSubmit = async (data) => {
+    try {
+      const response = await axios.post('http://localhost:3000/api/v1/email', data);
+  
+      if (response.status === 200) {
+        
+        alert('¡Correo electrónico enviado con éxito!');
+      } else {
+       
+        alert('Error al enviar el correo electrónico. Por favor, inténtalo de nuevo.');
+      }
+    } catch (error) {
+      console.error('Error al enviar el correo electrónico:', error);
+     
+      alert('Error al enviar el correo electrónico. Por favor, inténtalo de nuevo.');
+    }
+  };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="max-w-md mx-auto py-20">
@@ -17,24 +33,27 @@ function ContactForm() {
       <h3 className="text-sm mb-4">
         Coloca tus datos y qué preguntas quieres hacernos
       </h3>
-      {errors.firstName && <span>Este campo es obligatorio</span>}
+      {errors.firstName && <span style={{ color: "black" }}>Este campo es obligatorio</span>}
       <input
         type="text"
         placeholder="Nombre"
         {...register("firstName", { required: true })}
-        className="w-full px-3 py-2 mb-4 border border-gray-300 rounded-md"
+        className="w-1/2 px-3 py-2 mb-4 border border-gray-300 rounded-md"
+        style={{ color: "black" }}
       />
-      {errors.lastName && <span>Este campo es obligatorio</span>}
+      {errors.lastName && <span style={{ color: "black" }}>Este campo es obligatorio</span>}
       <input
         type="text"
         placeholder="Apellido"
         {...register("lastName", { required: true })}
-        className="w-full px-3 py-2 mb-4 border border-gray-300 rounded-md"
+        className="w-1/2 px-3 py-2 mb-4 border border-gray-300 rounded-md"
+        style={{ color: "black" }}
       />
-      {errors.interest && <span>Selecciona una opción</span>}
+      {errors.interest && <span style={{ color: "black" }}>Selecciona una opción</span>}
       <select
         {...register("interest", { required: true })}
         className="w-full px-3 py-2 mb-4 border border-gray-300 rounded-md"
+        style={{ color: "black" }}
       >
         <option value="">En qué estás interesado</option>
         <option value="rutinaPersonalizada">
@@ -45,17 +64,19 @@ function ContactForm() {
         </option>
         <option value="indumentaria">Indumentaria</option>
       </select>
-      {errors.email && <span>Ingresa un correo electrónico válido</span>}
+      {errors.email && <span style={{ color: "black" }}>Ingresa un correo electrónico válido</span>}
       <input
         type="email"
         placeholder="Email"
         {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
         className="w-full px-3 py-2 mb-4 border border-gray-300 rounded-md"
+        style={{ color: "black" }}
       />
       <textarea
         placeholder="Mensaje"
         {...register("message")}
         className="w-full px-3 py-2 mb-4 border border-gray-300 rounded-md"
+        style={{ color: "black" }}
       />
       <button type="submit" className="bg-blue-500  py-2 px-4 rounded-md">
         Enviar

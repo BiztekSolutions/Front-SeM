1145878d0df7a4780d3c5ab3299a1ccea15f66bd
Arian Calabrese <acalabrese@logytsud.com.ar>
2024-03-11 21:26:30 -0300

    utilities

--- frontend/src/utils/utilities.js
+++ frontend/src/utils/utilities.js
@@ -1,4 +1,4 @@
 //LOCALHOST DEV URL
-export const base_url = "http://localhost:3000/api/v1";
+//export const base_url = "http://localhost:3000/api/v1";
 //DEPLOYED URL
-// export const base_url = "https://sems-backend.herokuapp.com/api/v1";
+export const base_url = "https://mk3fm0s337.execute-api.us-east-1.amazonaws.com/dev/api/v1";
